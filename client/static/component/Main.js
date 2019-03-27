61b331c571e87a5a2e10b1003d11e37cf37c51b2
33 2 <790460820@qq.com>
2019-03-27 17:25:29 +0800

    2019.3.27 17:25

--- client/static/component/Main.js
+++ client/static/component/Main.js
@@ -3,52 +3,47 @@ import React, {Component, PropTypes} from 'react';
 class Header extends React.Component {
     render() {
       return (
-        <div class="header">
-      <section class="header-anchor">
-        <div class="header-titlebar">
-            <div class="header-search">
-              <div class="search-blank">
-                <input type="text" class="ipt" maxlength="50" autocomplete="off" placeholder="search">
-                <span class="search-btn"></span>
+        <header class="header">
+          <section  class="header-anchor">
+            <div class="header-titlebar">
+              <div class="header-search">
+                <div class="search-blank">
+                  <input type="text" class="ipt" maxlength="50" autocomplete="off" placeholder="search"></input>
+                  <span class="search-btn"></span>
+                </div>
               </div>
-              
+              <div class="header-logo"><h3>Cedar Blog</h3></div>
+              <div class="header-userinfo">
+                <div class="user-portrait">
+                  <a href="#"></a>
+                </div>
+                <ul class="social-links">
+                  <li class="qq">
+                    <a href="#"></a>
+                  </li>
+                  <li class="email">
+                    <a href="#"></a>
+                  </li>
+                  <li class="github">
+                    <a href="#"></a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div class="header-logo"><h3>Cedar Blog</h3></div>
-            <div class="header-userinfo">
-              <div class="user-portrait">
-                <a href="#"></a>
-              </div>
-              <ul class="social-links">
-                <li class="qq">
-                  <a href="#"></a>
-                </li>
-                <li class="email">
-                  <a href="#"></a>
-                </li>
-                <li class="github">
-                  <a href="#"></a>
-                </li>
+            <nav class="header-navbar">
+              <ul class="nav-menu">
+                <li class="menu-item"><a href="#">主页</a></li>
+                <li class="menu-item"><a href="#">学习</a></li>
+                <li class="menu-item"><a href="#">日记</a></li>
+                <li class="menu-item"><a href="#">相册</a></li>
+                <li class="menu-item"><a href="#">关于我</a></li>
               </ul>
+            </nav>
+          </section>
+          <div class="banner">
+            <h2>Welcome to my blog</h2>
           </div>
-        </div>
-  
-        <nav class="header-navbar">
-            <div class="header-blur-box blur-layer"></div>
-            <ul class="nav-menu">
-              <li class="menu-item"><a href="#">主页</a></li>
-              <li class="menu-item"><a href="#">学习</a></li>
-              <li class="menu-item"><a href="#">日记</a></li>
-              <li class="menu-item"><a href="#">相册</a></li>
-              <li class="menu-item"><a href="#">关于我</a></li>
-            </ul>
-        </nav>
-
-      </section>
-      <div class="banner">
-        <h2>Welcome to my blog</h2>
-      </div>
-
-    </div>
+        </header>
       );
     }
 }
