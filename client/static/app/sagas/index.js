acb24da90efbf24e6245371eefb8c0220330dd87
33 2 <790460820@qq.com>
2019-05-14 20:41:52 +0800

    2019.05.14

--- client/static/app/sagas/index.js
+++ client/static/app/sagas/index.js
@@ -1,8 +1,18 @@
+/*
 export default function * helloSaga() {
     console.log('Hello Sagas!');
 }
-
-/*
-import {fork} from 'redux-saga/effects'
-import {loginFlow, registerFlow, user_auth} from './homeSaga'
 */
+
+import {take,put,call} from 'redux-saga/effects'
+
+export function* getArticleList (tag,pageNum) {
+  yield put({type: IndexActionTypes.FETCH_START});
+  try {
+      return yield call(get, `/getArticles?pageNum=${pageNum}&isPublish=true&tag=${tag}`);
+  } catch (err) {
+      yield put({type: IndexActionTypes.SET_MESSAGE, msgContent: '网络请求错误', msgType: 0});
+  } finally {
+      yield put({type: IndexActionTypes.FETCH_END})
+  }
+}
